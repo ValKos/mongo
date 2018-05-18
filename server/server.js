5ef31581603e12e035999f3b07676424d5421ee4
unknown <6501764@gmail.com>
2018-05-18 22:55:02 +0300

    sdas

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 app.post ('/todos',(req,res) => {
   //console.log(req.body);
   var todo = new Todo ({
-    text: req.body.text
-    
+    text: req.body.text,
+    completed: req.body.completed
   })
   todo.save().then((doc) => {
     res.send(doc);
@@ -25,11 +25,23 @@ app.post ('/todos',(req,res) => {
 
 })
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos,
+      code: 'okay'
+    })
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
+
+module.exports = {app};
 
 
 
