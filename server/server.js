a609188631f82e36a6239f57ee5349c89585adf8
unknown <6501764@gmail.com>
2018-05-19 02:20:10 +0300

    af

--- server/server.js
+++ server/server.js
@@ -37,9 +37,23 @@ app.get('/todos', (req,res) => {
   })
 })
 
-app.get('/todos/:id',(req,res) => {
-  res.send(req.params)
-})
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 
 app.listen(port, () => {
