98f0e81cd58cde45d5e67520e6f8dca332df41cd
unknown <6501764@gmail.com>
2018-05-20 01:33:50 +0300

    del

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
@@ -61,11 +61,11 @@ app.delete('/todos/:id', (req,res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
-})
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
